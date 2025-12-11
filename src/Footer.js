36dba58bacfcbc9c26df29da3380f4265f774378
fayik.veznedaroglu <veznedaroglufayik2@gmail.com>
2025-12-11 19:01:57 +0300

    first_commit

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '1em', background: '#f1f1f1' }}>
+      <p>© 2024 My React App. All rights reserved.First Commit</p>
+    </footer>
+  );
+}
