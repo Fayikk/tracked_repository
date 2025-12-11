43c801ff7d4d72e0fe6cc090d3c168113901781b
fayik.veznedaroglu <veznedaroglufayik2@gmail.com>
2025-12-11 18:34:53 +0300

    deneme1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Deneme developmentbranch
+          Learn React Deneme developmentbranch deneme1
         </a>
       </header>
     </div>
